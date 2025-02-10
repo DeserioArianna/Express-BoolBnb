e8024c9ec7cd4db3ad0e4a0c0706c16345d62e35
Federico-Bargosi <bargossifede98@gmail.com>
2025-02-10 11:22:29 +0100

    Feat: aggiunta recensioni a show

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -18,21 +18,40 @@ const index = (req, res, next) => {
 const show = (req, res, next) => {
     const { id } = req.params;
 
-    const sql = `SELECT * FROM casa WHERE id = ?`;
+    const sql = `SELECT * FROM casa WHERE id = ?;`
+    const sqlReviews = `
+    SELECT recensioni.*
+    FROM recensioni
+    JOIN casa
+    ON casa.id = recensioni.id_casa
+    WHERE casa.id = ?`;
 
+    // PRIMA QUERY: Controllo se l'immobile esiste
     dbConnection.query(sql, [id], (err, results) => {
         if (err) {
-            return next(new Error("errore interno del server"))
+            return next(new Error("Errore interno del server"));
         }
         if (results.length === 0) {
             return res.status(404).json({ message: "Immobile non trovato" });
         }
-        return res.status(200).json({
-            status: "success",
-            data: results[0]
-        })
+
+        // SECONDA QUERY: Recupero le recensioni SOLO se l'immobile esiste
+        dbConnection.query(sqlReviews, [id], (err, reviews) => {
+            if (err) {
+                return next(new Error("Errore interno del server"));
+            }
+
+            return res.status(200).json({
+                status: "success",
+                data: {
+                    ...results[0], 
+                    reviews
+                }
+            });
+        });
     });
 };
+
 
 const store = (req, res, next) => {
     console.log("prova")
